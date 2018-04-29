53c382347cc8c64b49a9dd330a60e7d90cdffd68
Anams-106 <singh.anamika106@gmail.com>
2018-04-29 23:41:29 +0530

    first commit through android studio

--- lib/src/main/java/com/example/MyClassFactory.java
+++ lib/src/main/java/com/example/MyClassFactory.java
@@ -8,9 +8,9 @@ public class MyClassFactory {
         if (input.equalsIgnoreCase("A")) {
             return new SubA("A sub class", 3);
         } else if (input.equalsIgnoreCase("B")) {
-            return new SubB("A sub class", 2);
+            return new SubB("B sub class", 2);
         } else if (input.equalsIgnoreCase("C")) {
-            return new SubC("A sub class", 1);
+            return new SubC("C sub class", 1);
         } else {
             System.out.println("Enter valid inpput (a/b/c or A/B/C)");
             return null;
